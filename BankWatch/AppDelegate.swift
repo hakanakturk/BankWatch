8ae9b6547465277daf52b2f7b756a596daa0f284
Hakan Aktürk <hakanakturk@outlook.com>
2023-06-11 19:26:38 +0300

    tab bar added

--- BankWatch/AppDelegate.swift
+++ BankWatch/AppDelegate.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
+let appColor: UIColor = .systemTeal
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
-    
+            
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
+    let mainViewController = MainViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
@@ -24,11 +27,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate =  self
         onboardingContainerViewController.delegate = self
         dummyViewController.logoutDelegate = self
-        window?.rootViewController = loginViewController
+        
+        window?.rootViewController = mainViewController
 //        window?.rootViewController = onboardingContainerViewController
 //        window?.rootViewController = OnboardingContainerViewController()
 //        window?.rootViewController = OnboardingViewController(heroImageName: "delorean", titleText:  "BankWatch is fasti easier to use, and has a brand new look and feel that will make you feel like you are back in 1989")
         
+        mainViewController.selectedIndex = 1
         return true
     }
 }
@@ -55,7 +60,6 @@ extension AppDelegate: LoginViewControllerDelegate {
         } else {
             setRootViewController(onboardingContainerViewController)
         }
-        
     }
 }
 
---
+++ BankWatch/Utilities/UIViewController+Utils.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Utils.swift
+//  BankWatch
+//
+//  Created by Hakan Aktürk on 11.06.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground()
+        navBarAppearance.backgroundColor = appColor
+        
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
