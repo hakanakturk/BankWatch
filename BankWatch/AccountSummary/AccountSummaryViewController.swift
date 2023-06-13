e6644feca65182bf71dc34f760c3ff6fbabc701c
Hakan Aktürk <hakanakturk@outlook.com>
2023-06-13 03:29:11 +0300

    cell view is configured

--- BankWatch/AccountSummary/AccountSummaryViewController.swift
+++ BankWatch/AccountSummary/AccountSummaryViewController.swift
@@ -35,6 +35,8 @@ extension AccountSummaryViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
+        
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
         
@@ -59,8 +61,7 @@ extension AccountSummaryViewController {
 
 extension AccountSummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = games[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
         return cell
     }
     
